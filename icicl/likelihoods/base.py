6a2652b78ba850e024e9574ec6eafc8813177f00
MattAshman <matthew.ashman97@gmail.com>
2023-09-28 13:43:16 +0100

    Likelihood ABC

--- icicl/likelihoods/base.py
+++ icicl/likelihoods/base.py
@@ -1,10 +1,13 @@
+from abc import ABC, abstractmethod
+
 import torch
 import torch.distributions as td
 from torch import nn
 
 
-class Likelihood(nn.Module):
+class Likelihood(nn.Module, ABC):
     out_dim_multiplier = 1
 
+    @abstractmethod
     def forward(self, x: torch.Tensor) -> td.Distribution:
         raise NotImplementedError
