8aa211e243c9c25d00b8085fb771f987fe7baa32
MattAshman <matthew.ashman97@gmail.com>
2023-10-04 12:01:34 +0100

    Added parallel nested perceiver encoder

---
+++ icicl/models/plbanp.py
@@ -0,0 +1,50 @@
+import torch
+from check_shapes import check_shapes
+from torch import nn
+
+from ..networks.transformer import ParallelNestedPerceiverEncoder
+from .base import NeuralProcess
+from .lbanp import LBANPDecoder
+
+
+class PLBANPEncoder(nn.Module):
+    def __init__(
+        self,
+        parallel_nested_perceiver_encoder: ParallelNestedPerceiverEncoder,
+        xy_encoder: nn.Module,
+    ):
+        super().__init__()
+
+        self.parallel_nested_perceiver_encoder = parallel_nested_perceiver_encoder
+        self.xy_encoder = xy_encoder
+
+    @check_shapes(
+        "xc: [m, nc, dx]",
+        "yc: [m, nc, dy]",
+        "xt: [m, nt, dx]",
+        "return: [m, nq, dz]",
+    )
+    def forward(
+        self,
+        xc: torch.Tensor,
+        yc: torch.Tensor,
+        xt: torch.Tensor,
+    ) -> torch.Tensor:
+        zc = torch.cat((xc, yc), dim=-1)
+        zc = self.xy_encoder(zc)
+
+        zt = torch.cat((xt, torch.zeros(*xt.shape[:-1], yc.shape[-1])), dim=-1)
+        zt = self.xy_encoder(zt)
+
+        zt = self.parallel_nested_perceiver_encoder(zc, zt)
+        return zt
+
+
+class PLBANP(NeuralProcess):
+    def __init__(
+        self,
+        encoder: PLBANPEncoder,
+        decoder: LBANPDecoder,
+        likelihood: nn.Module,
+    ):
+        super().__init__(encoder, decoder, likelihood)
