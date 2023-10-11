23297ab8e4439946698fb23c12895ae80e51fe11
MattAshman <matthew.ashman97@gmail.com>
2023-10-11 14:57:35 +0100

    Added fmnist generator

---
+++ icicl/data/fmnist.py
@@ -0,0 +1,37 @@
+from abc import ABC
+
+import torchvision
+
+from .image import ICImageGenerator, ImageGenerator
+
+
+class FashionMNIST(ABC):
+    def __init__(self, data_dir: str, train: bool = True, download: bool = False):
+        self.dim = 28 * 28
+        self.dataset = torchvision.datasets.FashionMNIST(
+            root=data_dir,
+            train=train,
+            download=download,
+            transform=torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.ToTensor(),
+                    torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+                ]
+            ),
+        )
+
+
+class FashionMNISTGenerator(FashionMNIST, ImageGenerator):
+    def __init__(
+        self, *, data_dir: str, train: bool = True, download: bool = False, **kwargs
+    ):
+        FashionMNIST.__init__(self, data_dir, train, download)
+        ImageGenerator.__init__(self, dataset=self.dataset, dim=self.dim, **kwargs)
+
+
+class ICFashionMNISTGenerator(FashionMNIST, ICImageGenerator):
+    def __init__(
+        self, *, data_dir: str, train: bool = True, download: bool = False, **kwargs
+    ):
+        FashionMNIST.__init__(self, data_dir, train, download)
+        ICImageGenerator.__init__(self, dataset=self.dataset, dim=self.dim, **kwargs)
