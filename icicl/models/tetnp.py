8426042f84a15b53c2623798cb644af679b83018
MattAshman <matthew.ashman97@gmail.com>
2023-11-03 14:30:55 -0400

    Added EfficientTETNPD implementation

--- icicl/models/tetnp.py
+++ icicl/models/tetnp.py
@@ -2,7 +2,7 @@ import torch
 from check_shapes import check_shapes
 from torch import nn
 
-from ..networks.tetransformer import TETransformerEncoder
+from ..networks.tetransformer import TETNPDTransformerEncoder, TETransformerEncoder
 from ..utils.helpers import preprocess_observations
 from .base import NeuralProcess
 from .tnp import TNPDDecoder, gen_tnpd_mask
@@ -42,6 +42,42 @@ class TETNPD(NeuralProcess):
     def __init__(
         self,
         encoder: TETNPDEncoder,
+        decoder: TNPDDecoder,
+        likelihood: nn.Module,
+    ):
+        super().__init__(encoder, decoder, likelihood)
+
+
+class EfficientTETNPDEncoder(nn.Module):
+    def __init__(
+        self,
+        transformer_encoder: TETNPDTransformerEncoder,
+        y_encoder: nn.Module,
+    ):
+        super().__init__()
+
+        self.transformer_encoder = transformer_encoder
+        self.y_encoder = y_encoder
+
+    @check_shapes(
+        "xc: [m, nc, dx]", "yc: [m, nc, dy]", "xt: [m, nt, dx]", "return: [m, n, dz]"
+    )
+    def forward(
+        self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor
+    ) -> torch.Tensor:
+        yc, yt = preprocess_observations(xt, yc)
+
+        zc = self.y_encoder(yc)
+        zt = self.y_encoder(yt)
+
+        zt = self.transformer_encoder(zc, zt, xc, xt)
+        return zt
+
+
+class EfficientTETNPD(NeuralProcess):
+    def __init__(
+        self,
+        encoder: EfficientTETNPDEncoder,
         decoder: TNPDDecoder,
         likelihood: nn.Module,
     ):
