72e9c00d87b4c145364739fe1d370d77c119b53b
MattAshman <matthew.ashman97@gmail.com>
2024-01-06 13:03:23 +0000

    Updated eval.py to include plot_cru

--- experiments/eval.py
+++ experiments/eval.py
@@ -1,9 +1,11 @@
 import lightning.pytorch as pl
 from plot import plot
+from plot_cru import plot_cru
 from plot_image import plot_image
 from utils import initialize_evaluation, val_epoch
 
 import wandb
+from icicl.data.cru import CRUDataGenerator
 from icicl.data.image import ImageGenerator
 
 
@@ -31,6 +33,21 @@ def main():
             model=model,
             batches=batches,
             num_fig=min(experiment.misc.num_plots, len(batches)),
+            name="test",
+        )
+    elif isinstance(gen_val, CRUDataGenerator):
+        plot_cru(
+            model=model,
+            batches=batches,
+            x_mean=gen_val.x_mean,
+            x_std=gen_val.x_std,
+            y_mean=gen_val.y_mean,
+            y_std=gen_val.y_std,
+            num_fig=min(5, len(batches)),
+            figsize=(24.0, 4.0),
+            lat_range=gen_val.lat_range,
+            lon_range=gen_val.lon_range,
+            time_idx=(0, -1),
             name="test",
         )
     else:
