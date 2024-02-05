8e6af9dc3d64ced5cd562649568cdc61d98ce12b
MattAshman <matthew.ashman97@gmail.com>
2024-02-05 12:59:16 +0000

    Implemented Synthetic mixture generator

--- icicl/data/synthetic.py
+++ icicl/data/synthetic.py
@@ -1,7 +1,12 @@
+import operator
+import random
 from abc import ABC, abstractmethod
+from collections import Counter
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from functools import reduce
+from typing import List, Optional, Tuple
 
+import numpy as np
 import torch
 
 from .base import Batch, DataGenerator, GroundTruthPredictor, ICBatch
@@ -43,7 +48,7 @@ class SyntheticGenerator(DataGenerator, ABC):
         self.min_num_trg = torch.as_tensor(min_num_trg, dtype=torch.int)
         self.max_num_trg = torch.as_tensor(max_num_trg, dtype=torch.int)
 
-    def generate_batch(self) -> Batch:
+    def generate_batch(self, batch_shape: Optional[torch.Size] = None) -> Batch:
         """Generate batch of data.
 
         Returns:
@@ -57,6 +62,7 @@ class SyntheticGenerator(DataGenerator, ABC):
         batch = self.sample_batch(
             num_ctx=num_ctx,
             num_trg=num_trg,
+            batch_shape=batch_shape,
         )
 
         return batch
@@ -83,9 +89,13 @@ class SyntheticGenerator(DataGenerator, ABC):
 
         return num_ctx, num_trg
 
-    def sample_batch(self, num_ctx: int, num_trg: int) -> SyntheticBatch:
+    def sample_batch(
+        self, num_ctx: int, num_trg: int, batch_shape: Optional[torch.Size]
+    ) -> SyntheticBatch:
         # Sample inputs, then outputs given inputs
-        x = self.sample_inputs(num_ctx=num_ctx, num_trg=num_trg)
+        x = self.sample_inputs(
+            num_ctx=num_ctx, num_trg=num_trg, batch_shape=batch_shape
+        )
         y, gt_pred, _ = self.sample_outputs(x=x)
 
         xc = x[:, :num_ctx, :]
@@ -148,7 +158,7 @@ class ICSyntheticGenerator(SyntheticGenerator, ABC):
 
         self.ic_context_range = torch.as_tensor(ic_context_range, dtype=torch.float)
 
-    def generate_batch(self) -> ICBatch:
+    def generate_batch(self, batch_shape: Optional[torch.Size] = None) -> ICBatch:
         """Generate batch of data.
 
         Returns:
@@ -333,3 +343,80 @@ class ICSyntheticGeneratorBimodalInput(
     ICSyntheticGenerator, SyntheticGeneratorBimodalInput
 ):
     pass
+
+
+class SyntheticGeneratorMixture(SyntheticGenerator):
+    def __init__(
+        self,
+        *,
+        mix_samples: bool = False,
+        generators: Tuple[SyntheticGenerator, ...],
+        mixture_probs: Tuple[float, ...],
+        **kwargs,
+    ):
+        assert len(generators) == len(
+            mixture_probs
+        ), "Must be a mixture prob for each generator."
+        assert sum(mixture_probs) == 1, "Sum of mixture_probs must be 1."
+        assert all(
+            prob > 0 for prob in mixture_probs
+        ), "All elements of mixture_probs must be positive."
+
+        super().__init__(**kwargs)
+
+        # Whether or not to sample mixture for each sample in batch.
+        self.mix_samples = mix_samples
+        self.generators = generators
+        self.mixture_probs = mixture_probs
+
+        # Ensure samples per epoch of generators are infinite, so does not stop sampling.
+        for generator in self.generators:
+            generator.num_batches = np.inf
+
+    def generate_batch(self, batch_shape: Optional[torch.Size] = None) -> Batch:
+        if self.mix_samples:
+            # First, sample which generator the batch is sampled from.
+            gens = random.choices(
+                self.generators,
+                weights=self.mixture_probs,
+                k=self.batch_size,
+            )
+            gen_counter = Counter(gens)
+
+            # Sample nc and nt which is shared for all samples.
+            nc, nt = self.sample_num_ctx_trg()
+
+            sub_batches: List[Batch] = []
+            for gen, batch_size in gen_counter.items():
+                batch_shape = torch.Size((batch_size,))
+                sub_batch = gen.sample_batch(
+                    num_ctx=nc, num_trg=nt, batch_shape=batch_shape
+                )
+                sub_batches.append(sub_batch)
+
+            random.shuffle(sub_batches)
+
+            # Combine sub_batches.
+            # batch = sum(sub_batches)
+            batch = reduce(operator.add, sub_batches)
+            return batch
+
+        gen = random.choices(self.generators, weights=self.mixture_probs, k=1)[0]
+
+        # Sample nc and nt which is shared for all samples.
+        nc, nt = self.sample_num_ctx_trg()
+        return gen.sample_batch(
+            num_ctx=nc, num_trg=nt, batch_shape=torch.Size((self.batch_size,))
+        )
+
+    def sample_inputs(
+        self, num_ctx: int, num_trg: int, batch_shape: Optional[torch.Size] = None
+    ) -> torch.Tensor:
+        raise NotImplementedError
+
+    def sample_outputs(
+        self,
+        x: torch.Tensor,
+        xic: Optional[torch.Tensor] = None,
+    ) -> Tuple[torch.Tensor, Optional[GroundTruthPredictor], torch.Tensor]:
+        raise NotImplementedError
