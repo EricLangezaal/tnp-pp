6565137e1a079b8b01fced7e8d5dd21f56938fb3
MattAshman <matthew.ashman97@gmail.com>
2024-03-15 10:08:06 +0000

    Updated sensible ANP

--- icicl/models/anp.py
+++ icicl/models/anp.py
@@ -2,8 +2,12 @@ import torch
 from check_shapes import check_shapes
 from torch import nn
 
-from ..networks.attention_layers import MultiHeadAttentionLayer
+from ..networks.attention_layers import (
+    MultiHeadAttentionLayer,
+    MultiHeadCrossAttentionLayer,
+)
 from ..networks.transformer import TransformerEncoder
+from ..utils.helpers import preprocess_observations
 from .base import ConditionalNeuralProcess
 from .tnp import TNPDDecoder
 
@@ -39,8 +43,51 @@ class ANPEncoder(nn.Module):
         return zt
 
 
+class SensibleANPEncoder(nn.Module):
+    def __init__(
+        self,
+        transformer_encoder: TransformerEncoder,
+        mhca_layer: MultiHeadCrossAttentionLayer,
+        xy_encoder: TransformerEncoder,
+    ):
+        super().__init__()
+
+        self.transformer_encoder = transformer_encoder
+        self.mhca_layer = mhca_layer
+        self.xy_encoder = xy_encoder
+
+    @check_shapes(
+        "xc: [m, nc, dx]", "yc: [m, nc, dy]", "xt: [m, nt, dx]", "return: [m, n, dz]"
+    )
+    def forward(
+        self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor
+    ) -> torch.Tensor:
+        yc, yt = preprocess_observations(xt, yc)
+
+        zc = torch.cat((xc, yc), dim=-1)
+        zc = self.xy_encoder(zc)
+
+        zt = torch.cat((xt, yt), dim=-1)
+        zt = self.xy_encoder(zt)
+
+        # Self-attention layers on context tokens.
+        zc = self.transformer_encoder(zc)
+
+        # Cross-attention layer with input locations as query/keys.
+        zt = self.mhca_layer(zt, zc)
+
+        return zt
+
+
 class ANP(ConditionalNeuralProcess):
     def __init__(
         self, encoder: ANPEncoder, decoder: TNPDDecoder, likelihood: nn.Module
+    ):
+        super().__init__(encoder, decoder, likelihood)
+
+
+class SensibleANP(ConditionalNeuralProcess):
+    def __init__(
+        self, encoder: SensibleANPEncoder, decoder: TNPDDecoder, likelihood: nn.Module
     ):
         super().__init__(encoder, decoder, likelihood)
