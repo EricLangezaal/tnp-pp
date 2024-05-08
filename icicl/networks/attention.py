e0ce0cdd8a912f76137f00c767de6eca45a7b9d6
MattAshman <matthew.ashman97@gmail.com>
2024-05-08 21:40:29 +0100

    Use torch.nn.functional.scaled_dot_product_attention when we can

--- icicl/networks/attention.py
+++ icicl/networks/attention.py
@@ -1,4 +1,5 @@
 """https://github.com/rishikksh20/CrossViT-pytorch/blob/master/module.py"""
+
 from abc import ABC
 from typing import Optional
 
@@ -64,51 +65,59 @@ class BaseMultiHeadAttention(nn.Module, ABC):
         k = self.to_k(xk)
         v = self.to_v(xv)
 
-        # Each of shape (m, num_heads, n, head_dim).
-        q, k, v = map(
-            lambda t: einops.rearrange(t, "b n (h d) -> b h n d", h=self.num_heads),
-            (q, k, v),
-        )
-
-        # (m, num_heads, nq, nk).
-        dots = (q @ k.transpose(-1, -2)) * self.scale
-
-        if self.add_diagonal_attention:
-            assert (
-                xq.shape[-1] == xv.shape[-1]
-            ), "xq and xv must have same embedding dimension."
-            xq_k = self.to_k(xq)
-            xq_v = self.to_v(xq)
-            xq_k, xq_v = map(
-                lambda t: einops.rearrange(t, "b n (h d) -> b h n d", h=self.num_heads),
-                (xq_k, xq_v),
+        if not self.add_diagonal_attention and self.kernel is None:
+            out = nn.functional.scaled_dot_product_attention(
+                q, k, v, attn_mask=mask, scale=self.scale
             )
-            # Add diagonal self attention amongst xq.
-            diag_dots = (q * xq_k).sum(-1, keepdim=True) * self.scale
-
-            # (m, h, nq, nk + 1).
-            dots = torch.cat((dots, diag_dots), dim=-1)
-
-            # (m, h, nv + 1, head_dim).
-            # v = torch.cat((v, xq_v), dim=-2)
-
-        if self.kernel is not None:
-            dots = einops.rearrange(dots, "m h nq nk -> m nq nk h")
-            dots = self.kernel(dots)
-            dots = einops.rearrange(dots, "m nq nk h -> m h nq nk")
-
-        if mask is not None:
-            mask = einops.repeat(mask, "m n p -> m h n p", h=self.num_heads)
-            dots = torch.masked_fill(dots, mask, -float("inf"))
-
-        attn = dots.softmax(dim=-1)
-
-        if self.add_diagonal_attention:
-            out = attn[..., :-1] @ v + attn[..., -1:] * xq_v
         else:
-            out = attn @ v
+            # Each of shape (m, num_heads, n, head_dim).
+            q, k, v = map(
+                lambda t: einops.rearrange(t, "b n (h d) -> b h n d", h=self.num_heads),
+                (q, k, v),
+            )
 
-        out = einops.rearrange(out, "b h n d -> b n (h d)")
+            # (m, num_heads, nq, nk).
+            dots = (q @ k.transpose(-1, -2)) * self.scale
+
+            if self.add_diagonal_attention:
+                assert (
+                    xq.shape[-1] == xv.shape[-1]
+                ), "xq and xv must have same embedding dimension."
+                xq_k = self.to_k(xq)
+                xq_v = self.to_v(xq)
+                xq_k, xq_v = map(
+                    lambda t: einops.rearrange(
+                        t, "b n (h d) -> b h n d", h=self.num_heads
+                    ),
+                    (xq_k, xq_v),
+                )
+                # Add diagonal self attention amongst xq.
+                diag_dots = (q * xq_k).sum(-1, keepdim=True) * self.scale
+
+                # (m, h, nq, nk + 1).
+                dots = torch.cat((dots, diag_dots), dim=-1)
+
+                # (m, h, nv + 1, head_dim).
+                # v = torch.cat((v, xq_v), dim=-2)
+
+            if self.kernel is not None:
+                dots = einops.rearrange(dots, "m h nq nk -> m nq nk h")
+                dots = self.kernel(dots)
+                dots = einops.rearrange(dots, "m nq nk h -> m h nq nk")
+
+            if mask is not None:
+                mask = einops.repeat(mask, "m n p -> m h n p", h=self.num_heads)
+                dots = torch.masked_fill(dots, mask, -float("inf"))
+
+            attn = dots.softmax(dim=-1)
+
+            if self.add_diagonal_attention:
+                out = attn[..., :-1] @ v + attn[..., -1:] * xq_v
+            else:
+                out = attn @ v
+
+            out = einops.rearrange(out, "b h n d -> b n (h d)")
+
         out = self.to_out(out)
         return out
 
