49d92516538051909d26ef59086a8414ed7422db
Eric Langezaal <erl50@cam.ac.uk>
2024-05-28 11:22:40 +0100

    mhca encoder suggested changes and grid now actually listens to provided range

--- icicl/models/ootg_tnp.py
+++ icicl/models/ootg_tnp.py
@@ -5,6 +5,7 @@ import torch
 from torch import nn
 import einops
 import warnings
+from check_shapes import check_shapes
 
 from .base import OOTGConditionalNeuralProcess
 from .tnp import EfficientTNPDEncoder, TNPDDecoder
@@ -113,49 +114,61 @@ class OOTG_MHCA_TNPDEncoder(OOTG_TNPDEncoder):
 
         self.grid_mhca_layer = grid_mhca_layer
 
-    def grid_encode(self, zc_off_grid: torch.Tensor, zc_on_grid: torch.Tensor) -> torch.Tensor:
+    @check_shapes(
+        "xc_off_grid: [b, u, dx]", "xc_on_grid: [b, s, dx]", 
+        "zc_off_grid: [b, u, e]", "zc_on_grid: [b, s, e]", 
+        "return: [b, s, e]"
+    )
+    def grid_encode(self, xc_off_grid, xc_on_grid, zc_off_grid, zc_on_grid) -> torch.Tensor:
+        
         B, U, E = zc_off_grid.shape # 'U'nstructured
         S = zc_on_grid.shape[-2] # 'S'tructured
 
-        s_expanded = zc_on_grid.repeat(U, 1, 1, 1).movedim(0, 1) # (B, U, S, E)
-        u_expanded = zc_off_grid.repeat(S, 1, 1, 1).movedim(0, 2) # (B, U, S, E)
+        xc_on_grid = xc_on_grid.repeat(U, 1, 1, 1).movedim(0, 1) # (B, U, S, Xdim)
+        xc_off_grid = xc_off_grid.repeat(S, 1, 1, 1).movedim(0, 2) # (B, U, S, Xdim)
         
-        nearest_idx = (u_expanded - s_expanded).abs().sum(dim=-1).argmin(dim=2)
+        # shape (B, U): for each off_grid, tell me index of closest on_grid in same batch
+        nearest_idx = (xc_off_grid - xc_on_grid).abs().sum(dim=-1).argmin(dim=2)
 
+        # shape (B, U) or (B, S) respectively
+        # _batch_ first repeats batch number then increments, range first increments then repeats
         s_batch_idx = torch.arange(B).unsqueeze(-1).repeat(1, S)
         u_batch_idx = torch.arange(B).unsqueeze(-1).repeat(1, U)
         s_range_idx = torch.arange(S).repeat(B, 1)
         u_range_idx = torch.arange(U).repeat(B, 1)
 
+        # construct temporary mask that links each off-grid point to its closest on_grid point
         nearest_mask = torch.zeros(B, U, S, dtype=torch.int, device=zc_on_grid.device)
         nearest_mask[u_batch_idx, u_range_idx, nearest_idx] = 1
 
-        # add one for the on the grid point
+        # maximum nearest neigbours + add one for the on the grid point itself
         max_patch = nearest_mask.sum(dim=1).amax() + 1
-        # indexes show cumulative count (i.e. add one if element has occured before)
+        # batched cumulative count (i.e. add one if element has occured before)
+        # So [0, 0, 2, 0, 1, 2] -> [0, 1, 0, 2, 0, 1] along each batch
         cumcount_idx = (nearest_mask.cumsum(dim=1) - 1)[u_batch_idx, u_range_idx, nearest_idx]
 
-        # create tensor with for each grid-token all nearest off-grid + itself in third dimension
+        # create tensor with for each grid-token all nearest off-grid + itself in third axis
         joint_grid = torch.full((B, S, max_patch, E), self.FAKE_TOKEN, device=zc_on_grid.device)
+        # add nearest off the grid points to each on_the_grid point
         joint_grid[u_batch_idx, nearest_idx, cumcount_idx] = zc_off_grid[u_batch_idx, torch.arange(U)]
+        # add the on_the_grid points themselves at the end
         joint_grid[s_batch_idx, s_range_idx, -1] = zc_on_grid[s_batch_idx, torch.arange(S)]
         
         grid_stacked = einops.rearrange(joint_grid, "b s m e -> (b s) m e")
 
-        att_mask = torch.ones(B * S, max_patch, S, dtype=torch.bool, device=zc_on_grid.device)
-        # if fake token anywhere in embedding, ignore it
-        att_mask[grid_stacked.sum(-1) == self.FAKE_TOKEN, :] = False
+        att_mask = torch.ones(B * S, 1, max_patch, dtype=torch.bool, device=zc_on_grid.device)
+        # if fake token anywhere (sum with infinities stays infinity) in embedding, ignore it
+        att_mask[(grid_stacked.sum(-1) == self.FAKE_TOKEN).unsqueeze(1)] = False
 
-        # set fake value to something which won't overfloat attention calculation
+        # set fake value to something which won't overflow/NaN attention calculation
         grid_stacked[grid_stacked == self.FAKE_TOKEN] = 0
 
-        latents = self.latents.repeat(grid_stacked.size(0), 1, 1).to(zc_on_grid.device)
-        zc = self.grid_mhca_layer(latents, grid_stacked, mask=att_mask.transpose(-1, -2))
-
-        # TODO return shape is now (B, S, S, E) what do we do?
-        zc = einops.rearrange(zc, "(b ignore) s e -> b ignore s e", b=B)
-        # temporary
-        return zc[:, 0]
+        # repeat latents of shape (S, E) to (B * S, 1, E)
+        latents = self.latents.repeat(B, 1).unsqueeze(1).to(zc_on_grid.device)
+        zc = self.grid_mhca_layer(latents, grid_stacked, mask=att_mask)
+        # reshape output to match on_the_grid exactly again
+        zc = einops.rearrange(zc, "(b s) 1 e -> b s e", b=B)
+        return zc
        
     def forward( # picked apart to allow for embedding before gridding
         self,
@@ -177,7 +190,7 @@ class OOTG_MHCA_TNPDEncoder(OOTG_TNPDEncoder):
         zc_on_grid = torch.cat((xc_on_grid, yc_on_grid), dim=-1)
         zc_on_grid = self.xy_encoder(zc_on_grid)
 
-        zc = self.grid_encode(zc_off_grid=zc_off_grid, zc_on_grid=zc_on_grid)
+        zc = self.grid_encode(xc_off_grid=xc_off_grid, xc_on_grid=xc_on_grid, zc_off_grid=zc_off_grid, zc_on_grid=zc_on_grid)
 
         zt = torch.cat((xt, yt), dim=-1)
         zt = self.xy_encoder(zt)
@@ -212,6 +225,7 @@ class OOTG_ViTEncoder(OOTG_TNPDEncoder):
         z = self.patcher(z)
         # move 'channels' (i.e embed_dim) to end again
         z = z.movedim(1, -1)
+
         return flatten_grid(z)
     
 
