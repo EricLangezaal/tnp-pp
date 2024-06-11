23d4167123948ccf78ca3a5a474ac35a83d4bfe8
Eric Langezaal <erl50@cam.ac.uk>
2024-06-11 09:40:05 +0100

    copied proper headed attention and cleanup

--- icicl/networks/attention.py
+++ icicl/networks/attention.py
@@ -1,5 +1,3 @@
-"""https://github.com/rishikksh20/CrossViT-pytorch/blob/master/module.py"""
-
 from abc import ABC
 from typing import Optional
 
@@ -7,8 +5,6 @@ import einops
 import torch
 from check_shapes import check_shapes
 from torch import nn
-
-from .kernels import Kernel
 
 
 class BaseMultiHeadAttention(nn.Module, ABC):
@@ -19,8 +15,7 @@ class BaseMultiHeadAttention(nn.Module, ABC):
         num_heads: int,
         head_dim: int,
         p_dropout: float = 0.0,
-        kernel: Optional[Kernel] = None,
-        add_diagonal_attention: bool = False,
+        linear: bool = False,
     ):
         super().__init__()
 
@@ -41,11 +36,7 @@ class BaseMultiHeadAttention(nn.Module, ABC):
             else nn.Identity()
         )
 
-        # Optional kernel nonlinearity to apply to inner products.
-        self.kernel = kernel
-
-        # Whether to do diagonals in mhca attention.
-        self.add_diagonal_attention = add_diagonal_attention
+        self.linear = linear
 
     @check_shapes(
         "xq: [m, nq, dqk]",
@@ -65,59 +56,22 @@ class BaseMultiHeadAttention(nn.Module, ABC):
         k = self.to_k(xk)
         v = self.to_v(xv)
 
-        if not self.add_diagonal_attention and self.kernel is None:
-            out = nn.functional.scaled_dot_product_attention(
+        q, k, v = map(
+            lambda x: einops.rearrange(x, "m n (h d) -> m h n d", h=self.num_heads),
+            (q, k, v),
+        )
+
+        if mask is not None:
+            mask = einops.repeat(mask, "m n1 n2 -> m h n1 n2", h=self.num_heads)
+
+        if self.linear:
+            out = linear_attention(q, k, v, attn_mask=mask, scale=self.scale)
+        else:
+            out = nn.functional.scaled_dot_product_attention(  # pylint: disable=not-callable
                 q, k, v, attn_mask=mask, scale=self.scale
             )
-        else:
-            # Each of shape (m, num_heads, n, head_dim).
-            q, k, v = map(
-                lambda t: einops.rearrange(t, "b n (h d) -> b h n d", h=self.num_heads),
-                (q, k, v),
-            )
 
-            # (m, num_heads, nq, nk).
-            dots = (q @ k.transpose(-1, -2)) * self.scale
-
-            if self.add_diagonal_attention:
-                assert (
-                    xq.shape[-1] == xv.shape[-1]
-                ), "xq and xv must have same embedding dimension."
-                xq_k = self.to_k(xq)
-                xq_v = self.to_v(xq)
-                xq_k, xq_v = map(
-                    lambda t: einops.rearrange(
-                        t, "b n (h d) -> b h n d", h=self.num_heads
-                    ),
-                    (xq_k, xq_v),
-                )
-                # Add diagonal self attention amongst xq.
-                diag_dots = (q * xq_k).sum(-1, keepdim=True) * self.scale
-
-                # (m, h, nq, nk + 1).
-                dots = torch.cat((dots, diag_dots), dim=-1)
-
-                # (m, h, nv + 1, head_dim).
-                # v = torch.cat((v, xq_v), dim=-2)
-
-            if self.kernel is not None:
-                dots = einops.rearrange(dots, "m h nq nk -> m nq nk h")
-                dots = self.kernel(dots)
-                dots = einops.rearrange(dots, "m nq nk h -> m h nq nk")
-
-            if mask is not None:
-                mask = einops.repeat(mask, "m n p -> m h n p", h=self.num_heads)
-                dots = torch.masked_fill(dots, mask, -float("inf"))
-
-            attn = dots.softmax(dim=-1)
-
-            if self.add_diagonal_attention:
-                out = attn[..., :-1] @ v + attn[..., -1:] * xq_v
-            else:
-                out = attn @ v
-
-            out = einops.rearrange(out, "b h n d -> b n (h d)")
-
+        out = einops.rearrange(out, "m h n d -> m n (h d)")
         out = self.to_out(out)
         return out
 
@@ -177,3 +131,28 @@ class MultiHeadCrossAttention(BaseMultiHeadAttention):
         self, xq: torch.Tensor, xkv: torch.Tensor, mask: Optional[torch.Tensor] = None
     ):
         return super().propagate(xq, xkv, xkv, mask)
+
+
+@check_shapes(
+    "q: [m, h, nq, dqk]",
+    "k: [m, h, nkv, dqk]",
+    "v: [m, h, nkv, dq]",
+)
+def linear_attention(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    attn_mask: Optional[torch.Tensor],
+    scale: float = 1.0,
+):
+    if attn_mask is not None:
+        # TODO: What is going on here.
+        raise NotImplementedError("Not implemented yet.")
+
+    q = q.softmax(dim=-1)
+    k = k.softmax(dim=-1)
+    q = q * scale
+
+    kv = k.transpose(-1, -2) @ v
+    out = q @ kv
+    return out
