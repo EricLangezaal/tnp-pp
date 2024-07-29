673e41913b5c7cd922332f3fc94165708bac955c
EricLangezaal <eric.langezaal@gmail.com>
2024-07-29 15:29:03 +0200

    more sensible baseline

--- icicl/models/baseline.py
+++ icicl/models/baseline.py
@@ -1,19 +1,11 @@
-from typing import Tuple
-
+from scipy.interpolate import LinearNDInterpolator
+import numpy as np
 import torch
 from torch import nn
 
-from ..utils.grids import coarsen_grid, flatten_grid
 from ..data.on_off_grid import DataModality
 
 class InterpBaselineEncoder(nn.Module):
-
-    def __init__(
-            self,
-            interpolation_factors: Tuple[int,...],
-    ):
-        super().__init__()
-        self.interpolation_factors = interpolation_factors
 
     def forward(
         self, 
@@ -24,45 +16,20 @@ class InterpBaselineEncoder(nn.Module):
         xt: torch.Tensor,
         used_modality: DataModality = DataModality.BOTH,
     ) -> torch.Tensor:
-        grid_locations = flatten_grid(coarsen_grid(xc_on_grid, self.interpolation_factors))
-        grid_values = flatten_grid(coarsen_grid(yc_on_grid, self.interpolation_factors))
+        
+        xc = used_modality.get(xc_on_grid, xc_off_grid)
+        yc = used_modality.get(yc_on_grid, yc_off_grid)
 
-        B, U, Ydim = yc_off_grid.shape # 'U'nstructured
-        S = grid_values.shape[-2] # 'S'tructured
+        xc = xc.numpy(force=True) 
+        yc = yc.numpy(force=True)
+        xt = xt.numpy(force=True)
 
-        if used_modality == DataModality.ON_GRID:
-            avg_grid = grid_values
-        else:
-            # from PT Encoder make the NN tensor
-            nearest_idx = (
-                (xc_off_grid[..., None, :] - grid_locations[:, None, ...]).abs().sum(dim=-1).argmin(dim=2)
-            )
-            s_batch_idx = torch.arange(B).unsqueeze(-1).repeat(1, S)
-            u_batch_idx = torch.arange(B).unsqueeze(-1).repeat(1, U)
-            s_range_idx = torch.arange(S).repeat(B, 1)
-            u_range_idx = torch.arange(U).repeat(B, 1)
-            nearest_mask = torch.zeros(B, U, S, dtype=torch.int, device=xc_off_grid.device)
-            nearest_mask[u_batch_idx, u_range_idx, nearest_idx] = 1
-            
-            max_patch = nearest_mask.sum(dim=1).amax() + (used_modality == DataModality.BOTH)
-            cumcount_idx = (nearest_mask.cumsum(dim=1) - 1)[u_batch_idx, u_range_idx, nearest_idx]
-
-            joint_grid = torch.full((B, S, max_patch, Ydim), torch.nan, device=xc_off_grid.device)
-            joint_grid[u_batch_idx, nearest_idx, cumcount_idx] = yc_off_grid
-            if used_modality == DataModality.BOTH:
-                joint_grid[s_batch_idx, s_range_idx, -1] = grid_values
-
-            # for every coarsened location, average of grid points 
-            # and all nearest off grid points which aren't masked out
-            avg_grid = (joint_grid.nan_to_num()).sum(dim=2) / (~joint_grid.isnan()).sum(dim=2)
-
-        nearest_idx_t = (
-                (xt[..., None, :] - grid_locations[:, None, ...]).abs().sum(dim=-1).argmin(dim=2)
-            )
-        t_batch_idx = torch.arange(B).unsqueeze(-1).repeat(1, xt.shape[1])
-        interp_result = avg_grid[t_batch_idx, nearest_idx_t]
-
-        return interp_result
+        yts = []
+        for xc_b, yc_b, xt_b in zip(xc, yc, xt):
+            interp = LinearNDInterpolator(xc_b, yc_b, fill_value=yc.mean())
+            yts.append(interp(xt_b))
+        yt = torch.tensor(np.stack(yts, axis=0), device=xc_on_grid.device)
+        return yt
         
 
 class IdentityBaselineDecoder(nn.Module):
