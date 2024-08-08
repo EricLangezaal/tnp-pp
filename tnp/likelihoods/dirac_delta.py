fixed no gradient log prob

tnp/likelihoods/dirac_delta.py
@@ -1,9 +1,27 @@
 import torch
-import gpytorch.distributions as dist
+from torch import nn
+import torch.distributions as td
 
 from .base import Likelihood
 
+class CustomDelta(td.Distribution):
+    def __init__(self, output, **kwargs):
+        super().__init__(validate_args=False, **kwargs)
+        self.output = output
+
+    @property
+    def mean(self):
+        return self.output
+    
+    @property
+    def stddev(self):
+        return torch.zeros_like(self.output)
+    
+    def log_prob(self, value):
+       return nn.functional.mse_loss(self.output, value).sqrt()
+
+
 class DeltaLikelihood(Likelihood):
 
-    def forward(self, x: torch.Tensor) -> dist.delta.Delta:
-        return dist.delta.Delta(x)
+    def forward(self, x: torch.Tensor) -> CustomDelta:
+        return CustomDelta(x)